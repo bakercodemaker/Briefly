55fbd230a99510b5044d46890e3d9592792cc7f1
BakerBart <bakercodemaker@gmail.com>
2026-09-08 14:46:56 +0200

    test: align local runtime documentation contract

--- test/config/local_runtime_configuration_test.rb
+++ test/config/local_runtime_configuration_test.rb
@@ -9,6 +9,7 @@ class LocalRuntimeConfigurationTest < ActiveSupport::TestCase
     assert_match(/GEMINI_API_KEY.*optional/i, readme)
     assert_match(/docs\/screenshots\/locked-workspace\.png/, readme)
     assert_match(/docs\/screenshots\/completed-brief\.png/, readme)
-    assert_no_match(/docker compose|Render|Neon|DATABASE_URL/i, readme)
+    assert_match(/docker compose/i, readme)
+    assert_no_match(/Render|Neon|DATABASE_URL/i, readme)
   end
 end
